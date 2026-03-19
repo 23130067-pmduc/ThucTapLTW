dd3a2e05796d94565d5fc24bb2d8f0bec7a209f2
PhamVanViet23130380 <23130380@st.hcmuaf.edu.vn>
2026-03-19 21:47:24 +0700

    front phần thanh toán

--- src/main/java/vn/edu/nlu/fit/thuctapltw/Controller/loginController.java
+++ src/main/java/vn/edu/nlu/fit/thuctapltw/Controller/loginController.java
@@ -22,12 +22,7 @@ public class loginController extends HttpServlet {
     }
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = request.getSession(false);
-        if (session != null) {
-            session.invalidate();
-        }
-
-        response.sendRedirect(request.getContextPath() + "/trang-chu");
+        request.getRequestDispatcher("/login.jsp").forward(request, response);
     }
 
     @Override
