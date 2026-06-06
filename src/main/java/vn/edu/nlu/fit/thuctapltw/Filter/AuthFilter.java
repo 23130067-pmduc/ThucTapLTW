1494bffc78e75ac5ab14381ab695a08c6798072a
HuyHoang <23130115@st.hcmuaf.edu.vn>
2026-06-06 15:29:02 +0700

    feat: chặn truy cập các trang admin khi chưa đăng nhập

---
+++ src/main/java/vn/edu/nlu/fit/thuctapltw/Filter/AuthFilter.java
@@ -0,0 +1,49 @@
+package vn.edu.nlu.fit.thuctapltw.Filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import vn.edu.nlu.fit.thuctapltw.model.User;
+
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {
+        "/dashboard",
+        "/product-admin",
+        "/inventory-admin",
+        "/profit-report",
+        "/category-admin",
+        "/order-admin",
+        "/user-admin",
+        "/banner-admin",
+        "/news-admin",
+        "/notification-admin",
+        "/contact-admin",
+        "/admin-profile"
+})
+public class AuthFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        HttpSession session = request.getSession(false);
+
+        User user = null;
+        if (session != null) {
+            user = (User) session.getAttribute("userlogin");
+        }
+
+        if (user == null) {
+            response.sendRedirect(request.getContextPath() + "/login");
+            return;
+        }
+
+        chain.doFilter(req, res);
+    }
+}
